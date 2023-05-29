9cb4f99f492215c1be16618e3e7c8989dfc32e76
luthfisr <luthfi.ramadhan@edot.id>
2023-05-30 00:45:14 +0700

    add jwt config

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,16 +3,20 @@ import { AuthService } from './auth.service';
 import { UsersModule } from '../users/users.module';
 import { JwtModule } from '@nestjs/jwt';
 import { AuthController } from './auth.controller';
+import { ResponseService } from 'src/response/response.service';
+import { MessageService } from 'src/message/message.service';
+import { ConfigService } from '@nestjs/config';
 
 @Module({
   imports: [
+    ConfigService,
     UsersModule,
     JwtModule.register({
-      secret: process.env.AUTH_JWTSECRETKEY,
+      secret: `${process.env.AUTH_JWTSECRETKEY}`,
       signOptions: { expiresIn: '60s' },
     }),
   ],
-  providers: [AuthService],
+  providers: [AuthService, ResponseService, MessageService],
   controllers: [AuthController],
   exports: [AuthService],
 })
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,5 +1,9 @@
-import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { BadRequestException, HttpStatus, Injectable, UnauthorizedException } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
+import { compare } from 'bcrypt';
+import { MessageService } from 'src/message/message.service';
+import { RMessage } from 'src/response/response.interface';
+import { ResponseService } from 'src/response/response.service';
 import { UserService } from 'src/users/users.service';
 
 @Injectable()
@@ -7,16 +11,50 @@ export class AuthService {
   constructor(
     private usersService: UserService,
     private jwtService: JwtService,
+    private messageService: MessageService,
+    private responseService: ResponseService,
   ) {}
 
   async signIn(username, pass) {
     const user = await this.usersService.findOne(username);
-    if (user?.password !== pass) {
-      throw new UnauthorizedException();
+    const validate: boolean = await this.validatePassword(pass, user.password);
+    if (!validate) {
+      const errors: RMessage = {
+        value: pass,
+        property: 'password',
+        constraint: ['Invalid user accounts'],
+      };
+      throw new BadRequestException(
+        this.responseService.error(
+          HttpStatus.BAD_REQUEST,
+          errors,
+          'Bad Request',
+        ),
+      );
     }
-    const payload = { sub: user.id, username: user.username };
+
+    console.log('create token');
+    const payload = {
+      sub: user.id,
+      username: user.username,
+      user_type: user.role_name,
+    };
     return {
       access_token: await this.jwtService.signAsync(payload),
     };
+  }
+
+  async validatePassword(
+    passwordString: string,
+    passwordHash: string,
+  ): Promise<boolean> {
+    return this.bcryptComparePassword(passwordString, passwordHash);
+  }
+
+  async bcryptComparePassword(
+    passwordString: string,
+    passwordHashed: string,
+  ): Promise<boolean> {
+    return compare(passwordString, passwordHashed);
   }
 }
